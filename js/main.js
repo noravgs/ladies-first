b6e1f1c299d86409043a780da6e27c2cf0355c95
a-cole55 <arniqua_c@yahoo.com>
2022-06-05 01:29:06 -0500

    added styling changes

--- js/main.js
+++ js/main.js
@@ -7,12 +7,15 @@ async function apiRequest(){
         const data = await response.json()
 
         console.log(data)
+        document.querySelector('video').classList = ""
         document.querySelector('#name').innerText = data.name
-        document.querySelector('p').innerText = data.lyric
+        document.querySelector('p').innerText = `" ${data.lyric} "`
         document.querySelector('.song').innerText = data.song
-        document.querySelector('#location').innerText = data.location
-        document.querySelector('#dob').innerText = data.dob
-        document.querySelector('#age').innerText = data.age
+        document.querySelector('#location').innerText = `Hometown: ${data.location}`
+        document.querySelector('#dob').innerText = `DOB: ${data.dob}`
+        document.querySelector('#age').innerText = `Age: ${data.age}`
+        document.querySelector('video').src = data.video
+
 
     }catch(error){
         console.log(error)
